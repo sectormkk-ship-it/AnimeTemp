0ff25a29a84744c4fb4a0c5f13945c33afd946f2
Nicolas Aguirre <sectormkk@gmail.com>
2026-05-06 09:43:06 -0300

    fix asgi settings render

--- anime_catalogo/asgi.py
+++ anime_catalogo/asgi.py
@@ -7,7 +7,7 @@ from django.core.asgi import get_asgi_application
 
 from catalogo.routing import websocket_urlpatterns
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "anime_catalogo.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE",os.environ.get("DJANGO_SETTINGS_MODULE", "anime_catalogo.settings"))
 django.setup()
 
 application = ProtocolTypeRouter({
