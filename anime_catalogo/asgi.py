248a34263ac09478e73cecadb3dcad8c89e6f30f
Nicolas Aguirre <sectormkk@gmail.com>
2026-05-06 10:03:20 -0300

    fix orden asgi definitivo

--- anime_catalogo/asgi.py
+++ anime_catalogo/asgi.py
@@ -1,28 +1,25 @@
 import os
 import django
 import os
-import django
 
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from django.core.asgi import get_asgi_application
-
-from catalogo.routing import websocket_urlpatterns
-
-# 🔥 IMPORTANTE
+# 🔥 IMPORTANTE → esto primero
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
     os.environ.get("DJANGO_SETTINGS_MODULE", "anime_catalogo.settings")
 )
 
+import django
 django.setup()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from django.core.asgi import get_asgi_application
+from catalogo.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
 
     "websocket": AuthMiddlewareStack(
-        URLRouter(
-            websocket_urlpatterns
-        )
+        URLRouter(websocket_urlpatterns)
     ),
 })
